924a56a10e7fc1a82b1785c38b04f6bda4dbdd21
Arslanarsal <arslanarsal455@gmail.com>
2026-06-13 02:45:54 +0500

    add the http layer

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -2,40 +2,66 @@ import express from 'express'
 import { loginmiddleware } from './middleware/loginMiddleware';
 import jwt from 'jsonwebtoken';
 import { JWT_TOKEN } from '@repo/backend-common/config';
-import {createUserScheme  , signinScheme , createroom} from "@repo/common/types"
+import { createUserScheme, signinScheme, createroom } from "@repo/common/types"
+import { prisma } from '@repo/db/config';
 
 const app = express();
 
-app.get('/login', (req, res) => {
-   const data  = signinScheme.safeParse(req.body);
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+
+    const data = createUserScheme.safeParse(req.body);
     if (!data.success) {
-        res.status(400).json({ error: data.error });
+        res.status(400).json({ error: data.error.message });
         return;
     }
-   const userId = "12345"; 
-   const token = jwt.sign(userId , JWT_TOKEN);
-   res.json({ token });
+    const user = await prisma.user.create({
+        data: {
+            name: data.data.name,
+            password: data.data.password
+        }
+    })
+    res.json({ user });
 })
 
-app.get('/sigup', (req, res) => {
-
-    const data  = createUserScheme.safeParse(req.body);
+app.post('/login', async (req, res) => {
+    const data = signinScheme.safeParse(req.body);
     if (!data.success) {
         res.status(400).json({ error: data.error });
         return;
     }
-   // db call to create user
+    const user = await prisma.user.findFirst({
+        where: {
+            name: data.data.name,
+            password: data.data.password
+        }
+    })
+
+    if (!user) {
+        res.status(401).json({ error: "Invalid username or password" });
+        return;
+    }
+
+    const token = jwt.sign(user?.id as unknown as String, JWT_TOKEN);
+    res.json({ token });
 })
 
-app.get('/room', loginmiddleware ,(req, res) => {
-    const data  = createroom.safeParse(req.body);
+app.post('/room', loginmiddleware, async (req, res) => {
+    const data = createroom.safeParse(req.body);
     if (!data.success) {
         res.status(400).json({ error: data.error });
         return;
     }
+    await prisma.room.create({
+        data: {
+            slug: data.data.name,
+            userId: Number(data.data.userId),
+        }
+    })
     res.send("room created")
 })
 
-app.listen(3001 , ()=>{
+app.listen(3001, () => {
     console.log("Server is running on port 3001");
 })
---
+++ packages/db/prisma/migrations/20260611213437_initial_schema/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" SERIAL NOT NULL,
+    "message" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" SERIAL NOT NULL,
+    "slug" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "chatId" INTEGER NOT NULL,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Room_slug_key" ON "Room"("slug");
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Room" ADD CONSTRAINT "Room_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Room" ADD CONSTRAINT "Room_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
